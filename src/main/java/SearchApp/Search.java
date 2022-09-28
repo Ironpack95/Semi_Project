d63ce47d3e17bdc3cf2fd4ed3ac391c6835c7eb0
Jongho Song <86420813+Ironpack95@users.noreply.github.com>
2022-09-28 20:19:36 +0900

    Update Search.java

--- src/main/java/SearchApp/Search.java
+++ src/main/java/SearchApp/Search.java
@@ -3,7 +3,6 @@ package SearchApp;
 public class Search {
 
 	// 쿼리문 변수
-	// ori_name, sys_name 추가예정
 	private String defaultSql = "select row_number() over(order by product_name desc) num, product_name, seq, kind, price, abv, grade, smry, ori_name, sys_name from product_info where"; // index추가
 
 	private String pname = " product_name like '%'||?||'%' ";
@@ -11,9 +10,9 @@ public class Search {
 	private String achType = " (kind like ? or kind like ? or kind like ? or kind like ? or kind like ?) ";
 
 	private String area = " (product_area like ? or product_area like ? or product_area like ? or product_area like ? or product_area like ? or product_area like ?) ";
-	//	String area = "product_area like '경기도' or product_area like '경상도' or product_area like '강원도' or product_area like '전라도' or product_area like '충청도' or product_area like '제주도'";
-
+	
 	private String gradeStr = " (grade between ? and ? + 0.9) ";
+	
 	private String abvStr = " (abv >= ?)";
 
 	// 대치 쿼리문
@@ -21,11 +20,10 @@ public class Search {
 
 	//	boolean[] searchType = new boolean[] {true, false, false, true, true};
 
-
 	// 쿼리문 반환 
 	public String getSql(boolean[] searchType) {
 
-		// 쿼리문 공장 돌리기
+		// 쿼리문 연결
 		for (int i=0; i<searchType.length; i++) {
 
 			if(searchType[i]) {
@@ -52,8 +50,6 @@ public class Search {
 				}
 			}
 		}
-		
-//		System.out.println(defaultSql.contains("whereand"));
 		
 		if (defaultSql.contains("whereand")) {
 			
